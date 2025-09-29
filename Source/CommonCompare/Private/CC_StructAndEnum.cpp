56b5c23ad8453fc55c44038de43ac972c04da655
LLWIFI <1204717710@qq.com>
2025-09-29 18:55:33 +0800

    国庆版本
    对每个可对比的类型再细分了All 和Any

--- Source/CommonCompare/Private/CC_StructAndEnum.cpp
+++ Source/CommonCompare/Private/CC_StructAndEnum.cpp
@@ -17,6 +17,7 @@ bool FCC_BeCompareInfo::CompareResult(FCC_CompareInfo& OuterCompareInfo, FText& 
 	}
 	else
 	{
+		bool b = false;
 		//Tag对照
 		if (!CompareTag_AllExact.IsEmpty())
 		{
@@ -24,7 +25,7 @@ bool FCC_BeCompareInfo::CompareResult(FCC_CompareInfo& OuterCompareInfo, FText& 
 			{
 				if (!OuterCompareInfo.CompareTag.GetGameplayTagArray().Contains(OtherTag))//外部是不是每个Tag都包含
 				{
-					FailText = CompareFailText.GetFailText_TagAllExact(OtherTag);
+					FailText = CompareFailText.GetFailText_TagAllExact();
 					return false;
 				}
 			}
@@ -45,22 +46,159 @@ bool FCC_BeCompareInfo::CompareResult(FCC_CompareInfo& OuterCompareInfo, FText& 
 			return false;
 		}
 		//Class对照
-		if (!CompareClass.IsNull() && CompareClass != OuterCompareInfo.CompareClass && !(OuterCompareInfo.CompareObject && OuterCompareInfo.CompareObject->IsA(CompareClass.Get())))
+		TArray<TSubclassOf<UObject>> CompareClassArray = OuterCompareInfo.CompareClass;
+		if (OuterCompareInfo.CompareObject.Num() > 0)//整理全部class
 		{
-			FailText = CompareFailText.GetFailText_Class();
-			return false;
+			for (UObject*& obj : OuterCompareInfo.CompareObject)
+			{
+				CompareClassArray.Add(obj->GetClass());
+			}
+		}
+
+		if (CompareClass_All.Num() > 0)
+		{
+			for (TSoftClassPtr<UObject>& CompareClass : CompareClass_All)
+			{
+				if (!CompareClassArray.Contains(CompareClass.LoadSynchronous()))//全部匹配，不允许有一个不包含
+				{
+					FailText = CompareFailText.GetFailText_ClassAll();
+					return false;
+				}
+			}
+		}
+		if (CompareClass_Any.Num() > 0)
+		{
+			b = false;
+			for (TSoftClassPtr<UObject>& CompareClass : CompareClass_Any)
+			{
+				if (CompareClassArray.Contains(CompareClass.LoadSynchronous()))//外部有任意一个即可
+				{
+					b = true;
+					break;
+				}
+			}
+			if (!b)
+			{
+				FailText = CompareFailText.GetFailText_ClassAny();
+				return false;
+			}
+		}
+		if (CompareClass_AllChild.Num() > 0)//包含子类的全部匹配，不允许有一个不包含
+		{
+			for (TSoftClassPtr<UObject>& CompareClass : CompareClass_AllChild)
+			{
+				b = false;
+				for (TSubclassOf<UObject>& OutClass : CompareClassArray)
+				{
+					if (OutClass && OutClass->IsChildOf(CompareClass.LoadSynchronous()))
+					{
+						b = true;
+						break;
+					}
+				}
+				if (!b)
+				{
+					FailText = CompareFailText.GetFailText_ClassAllChild();
+					return false;
+				}
+			}
+		}
+		if (CompareClass_AnyChild.Num() > 0)//包含子类的匹配，外部只用满足任意一个即可
+		{
+			b = false;
+			for (TSoftClassPtr<UObject>& CompareClass : CompareClass_AnyChild)
+			{
+				for (TSubclassOf<UObject>& OutClass : CompareClassArray)
+				{
+					if (OutClass && OutClass->IsChildOf(CompareClass.LoadSynchronous()))
+					{
+						b = true;
+						break;
+					}
+				}
+				if (b)
+				{
+					break;
+				}
+			}
+			if (!b)
+			{
+				FailText = CompareFailText.GetFailText_ClassAnyChild();
+				return false;
+			}
 		}
 		//String对照
-		if (!CompareString.IsEmpty() && CompareString != OuterCompareInfo.CompareString)
+		if (CompareString_All.Num() > 0)
 		{
-			FailText = CompareFailText.GetFailText_String();
-			return false;
+			for (FString& s : CompareString_All)
+			{
+				if (!OuterCompareInfo.CompareString.Contains(s))//全部匹配，不允许有一个不包含
+				{
+					FailText = CompareFailText.GetFailText_StringAll();
+					return false;
+				}
+			}
+		}
+		if (CompareString_Any.Num() > 0)
+		{
+			b = false;
+			for (FString& s : CompareString_Any)
+			{
+				if (OuterCompareInfo.CompareString.Contains(s))//外部有任意一个即可
+				{
+					b = true;
+					break;
+				}
+			}
+			if (!b)
+			{
+				FailText = CompareFailText.GetFailText_StringAny();
+				return false;
+			}
 		}
 		//Float对照
-		if (!CompareFloat.Contains(OuterCompareInfo.CompareFloat))
+
+		if (CompareFloat_All.HasLowerBound() || CompareFloat_All.HasUpperBound())
 		{
-			FailText = CompareFailText.GetFailText_Float();
-			return false;
+			b = false;
+			for (float& f : OuterCompareInfo.CompareFloat)
+			{
+				if (CompareFloat_All.Contains(f))
+				{
+					b = true;
+					break;
+				}
+			}
+			if (!b)
+			{
+				FailText = CompareFailText.GetFailText_FloatAll();
+				return false;
+			}
+		}
+
+		if (CompareFloat_Any.Num() > 0)
+		{
+			b = false;
+			for (float& f : OuterCompareInfo.CompareFloat)
+			{
+				for (FFloatRange FloatRange : CompareFloat_Any)
+				{
+					if (FloatRange.Contains(f))
+					{
+						b = true;
+						break;
+					}
+				}
+				if (b)
+				{
+					break;
+				}
+			}
+			if (!b)
+			{
+				FailText = CompareFailText.GetFailText_FloatAny();
+				return false;
+			}
 		}
 	}
 	return true;
--- Source/CommonCompare/Public/CC_StructAndEnum.h
+++ Source/CommonCompare/Public/CC_StructAndEnum.h
@@ -11,9 +11,18 @@ class UCC_CompareBase;
 * 该结构体将常见的比较情况进行了汇总
 */
 USTRUCT(BlueprintType)
-struct FCC_CompareInfo
+struct COMMONCOMPARE_API FCC_CompareInfo
 {
 	GENERATED_BODY()
+public:
+	FCC_CompareInfo Append(FCC_CompareInfo& CompareInfo)
+	{
+		CompareTag.AppendTags(CompareInfo.CompareTag);
+		CompareClass.Append(CompareInfo.CompareClass);
+		CompareString.Append(CompareInfo.CompareString);
+		CompareFloat.Append(CompareInfo.CompareFloat);
+		return *this;
+	}
 
 public:
 
@@ -25,41 +34,33 @@ public:
 	* 和被比对（FCC_BeCompareInfo）信息比较时该Class = FCC_BeCompareInfo.CompareClass 或 CompareObject的类型为FCC_BeCompareInfo.CompareClass的子类
 	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSoftClassPtr<UObject> CompareClass;
+	TArray<TSubclassOf<UObject>> CompareClass;
 	/*对照Obejct信息
 	* 该值和被比对（FCC_BeCompareInfo）信息比较时该值的类型为FCC_BeCompareInfo.CompareClass的子类时可以通过calss的比对
 	* 该值也可以用来判断一些更加细致的具体事项（例如复数信息），例如目标是否携带某种状态，交互的单位身价是否超过某个数值
 	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UObject* CompareObject;
+	TArray<UObject*> CompareObject;
 
 	//对照字符
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString CompareString;
+	TArray<FString> CompareString;
 
 	//对照数值
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float CompareFloat;
+	TArray<float> CompareFloat;
 };
 
 /*对比失败的文本
 */
 USTRUCT(BlueprintType)
-struct FCC_CompareFailText
+struct COMMONCOMPARE_API FCC_CompareFailText
 {
 	GENERATED_BODY()
 public:
-	FText GetFailText_TagAllExact(FGameplayTag Tag)
+	FText GetFailText_TagAllExact()
 	{
-		if (IsUseUnifiedText)
-		{
-			return FailText;
-		}
-		else if(CompareTag_AllExact_FailText.Contains(Tag))
-		{
-			return CompareTag_AllExact_FailText[Tag];
-		}
-		return FText();
+		return IsUseUnifiedText ? FailText : CompareTag_AllExact_FailText;
 	};
 	FText GetFailText_TagAnyExact()
 	{
@@ -74,19 +75,39 @@ public:
 		return IsUseUnifiedText ? FailText : CompareTag_Any_FailText;
 	};
 
-	FText GetFailText_Class()
+	FText GetFailText_ClassAll()
 	{
-		return IsUseUnifiedText ? FailText : CompareClass_FailText;
+		return IsUseUnifiedText ? FailText : CompareClass_All_FailText;
+	};
+	FText GetFailText_ClassAny()
+	{
+		return IsUseUnifiedText ? FailText : CompareClass_Any_FailText;
+	};
+	FText GetFailText_ClassAllChild()
+	{
+		return IsUseUnifiedText ? FailText : CompareClass_AllChild_FailText;
+	};
+	FText GetFailText_ClassAnyChild()
+	{
+		return IsUseUnifiedText ? FailText : CompareClass_AnyChild_FailText;
 	};
 
-	FText GetFailText_String()
+	FText GetFailText_StringAll()
 	{
-		return IsUseUnifiedText ? FailText : CompareString_FailText;
+		return IsUseUnifiedText ? FailText : CompareString_All_FailText;
+	};
+	FText GetFailText_StringAny()
+	{
+		return IsUseUnifiedText ? FailText : CompareString_Any_FailText;
 	};
 
-	FText GetFailText_Float()
+	FText GetFailText_FloatAll()
 	{
-		return IsUseUnifiedText ? FailText : CompareFloat_FailText;
+		return IsUseUnifiedText ? FailText : CompareFloat_All_FailText;
+	};
+	FText GetFailText_FloatAny()
+	{
+		return IsUseUnifiedText ? FailText : CompareFloat_Any_FailText;
 	};
 public:
 	//是否使用统一文本
@@ -97,10 +118,9 @@ public:
 	FText FailText;
 
 	/*Tag比对失败的文本。外部需要全部匹配，且Tag需要完全/精准匹配
-	* <哪个Tag没通过，没通过时的文本提示>
 	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
-	TMap<FGameplayTag, FText> CompareTag_AllExact_FailText;
+	FText CompareTag_AllExact_FailText;
 
 	//Tag比对失败的文本/提示。外部只用满足任意一个即可，且Tag需要完全/精准匹配
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
@@ -115,24 +135,39 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
 	FText CompareTag_Any_FailText;
 
-	//Class比对失败的文本/提示
+	//Class比对失败的文本/提示。外部需要全部匹配，class必须完全匹配
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
-	FText CompareClass_FailText;
+	FText CompareClass_All_FailText;
+	//Class比对失败的文本/提示。外部只用满足任意一个即可，class必须完全匹配
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
+	FText CompareClass_Any_FailText;
+	//Class比对失败的文本/提示。外部需要全部匹配，class的判断包含子类
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
+	FText CompareClass_AllChild_FailText;
+	//Class比对失败的文本/提示。外部只用满足任意一个即可，class的判断包含子类
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
+	FText CompareClass_AnyChild_FailText;
 
-	//字符比对失败的文本/提示
+	//字符比对失败的文本/提示。外部需要全部匹配
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
-	FText CompareString_FailText;
+	FText CompareString_All_FailText;
+	//字符比对失败的文本/提示。外部只用满足任意一个即可
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
+	FText CompareString_Any_FailText;
 
-	//数值比对失败的文本/提示
+	//数值比对失败的文本/提示。外部需要全部匹配
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
-	FText CompareFloat_FailText;
+	FText CompareFloat_All_FailText;
+	//数值比对失败的文本/提示。外部只用满足任意一个即可
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseUnifiedText"))
+	FText CompareFloat_Any_FailText;
 };
 
 /*被对比对照结构体
 * 该结构体将常见的比较情况进行了汇总
 */
 USTRUCT(BlueprintType)
-struct FCC_BeCompareInfo : public FTableRowBase
+struct COMMONCOMPARE_API FCC_BeCompareInfo : public FTableRowBase
 {
 	GENERATED_BODY()
 public:
@@ -171,23 +206,52 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Categories = "Compare"), meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
 	FGameplayTagContainer CompareTag_Any;
 
-	/*对照Class 会判断是否等于该类或该类的子类
+	/*对照Class 这里配置的class外部需要全部匹配，class必须完全匹配
 	* 例如判断持有的道具是否是某个Class的子类
 	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
-	TSoftClassPtr<UObject> CompareClass;
+	TArray<TSoftClassPtr<UObject>> CompareClass_All;
+	/*对照Class 这里配置的class外部只用满足任意一个即可，class必须完全匹配
+	* 例如判断持有的道具是否是某个Class的子类
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
+	TArray<TSoftClassPtr<UObject>> CompareClass_Any;
+	/*对照Class 这里配置的class外部需要全部匹配，class的判断包含子类
+	* 例如判断持有的道具是否是某个Class的子类
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
+	TArray<TSoftClassPtr<UObject>> CompareClass_AllChild;
+	/*对照Class 这里配置的class外部只用满足任意一个即可，class的判断包含子类
+	* 例如判断持有的道具是否是某个Class的子类
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
+	TArray<TSoftClassPtr<UObject>> CompareClass_AnyChild;
 
 	/*对照字符比对
 	* 某些不至于使用UObject但是Class不足以判断内容时，可以选择使用该内容进行判断
 	* 例如：目标是否携带某种状态也可以通过定于该值进行判断：例如用Fire代表燃烧；Ice代表冰冻
+	* 这里的配置外部需要全部匹配
 	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
-	FString CompareString;
+	TArray<FString> CompareString_All;
+	/*对照字符比对
+	* 某些不至于使用UObject但是Class不足以判断内容时，可以选择使用该内容进行判断
+	* 例如：目标是否携带某种状态也可以通过定于该值进行判断：例如用Fire代表燃烧；Ice代表冰冻
+	* 这里的配置外部只用满足任意一个即可
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
+	TArray<FString> CompareString_Any;
 
 	/*数值对比
+	* 这里的配置外部需要全部匹配
 	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
-	FFloatRange CompareFloat;
+	FFloatRange CompareFloat_All;
+	/*数值对比
+	* 这里的配置外部只用满足任意一个即可
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditionHides, EditCondition = "!IsUseOuterCompareClass"))
+	TArray<FFloatRange> CompareFloat_Any;
 
 	/*对比失败的文本
 	*/
